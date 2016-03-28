6d6f06c05415cefc324d7b3272607780f9be4c91
Jonas Bastos <jonasbastos96@gmail.com>
2016-03-28 20:07:59 -0300

    First two classes.

---
+++ src/model/page.py
@@ -0,0 +1,10 @@
+class Page:
+    def __init__(self):
+        url = None
+        head_tag = None
+        text_tag = None
+        subtext_tag = None
+        image_tag = None
+        image_caption_tag = None
+        author_tag = None
+        date_tag = None
---
+++ src/model/subdomain.py
@@ -0,0 +1,5 @@
+class Subdomain:
+    def __init__(self):
+        url = None
+        pages_id = None
+        pages = None
