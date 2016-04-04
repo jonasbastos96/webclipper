658e078280397ece4cf4f142d07997422686abe0
Jonas Bastos <jonasbastos96@gmail.com>
2016-04-03 21:18:24 -0300

    Update attributes on structure model. Also updates the database.

--- webclipper/model/structure.py
+++ webclipper/model/structure.py
@@ -7,11 +7,14 @@ class Structure:
     """
 
     def __init__(self):
+        self.title_tag = str()
         self.heading_tag = str()
-        self.subheading_tag = str()
         self.text_tag = str()
         self.subtext_tag = str()
         self.image_tag = str()
         self.caption_tag = str()
-        self.author_tag = str()
-        self.date_tag = str()
+        self.title_path = str()
+        self.author_path = str()
+        self.date_path = str()
+        self.content_path = str()
+        self.date_format = str()
